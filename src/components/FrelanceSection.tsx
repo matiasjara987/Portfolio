6f2636da974ba481546b33d76f8ab7954c5e09ed
Matias jara <matiasjats@gmail.com>
2025-05-09 09:42:26 -0400

    Creando dos nuevas funciones

---
+++ src/components/FrelanceSection.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "./ui/card";
+  import { Github } from "lucide-react";
+  import { GlassCard } from "./ui/glass-card";
+  import MotionWrapper from "./MotionWrapper";
+  import { motion } from "framer-motion";
+  
